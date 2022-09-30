2e00dcc5b25d54ff15aec6b9752be61e14b3ffc8
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2022-09-30 17:14:06 -0300

    criei menu normal e menu mobile responsivo e com acessibilidade

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+  if(event.type === 'touchstart') event.preventDefault(); //Eliminar os 2 clicks do touchstart
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
+
+  //acessibilidade em relação a expanção do menu
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+
+  //trocar mensagem de abrir menu para fechar menu aria-label
+  if(active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar-menu');
+  }else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir-menu');
+  }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
