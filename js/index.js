2910d6220d6c084cf220d203759faf85c0fca770
ygorp <ygor-nogueira@hotmail.com>
2022-12-13 10:22:55 -0300

    criando função para email

--- js/index.js
+++ js/index.js
@@ -37,5 +37,37 @@ btnAbout.addEventListener('click', orcamento);
 homecell.addEventListener('click', linkBtn1);
 comunica.addEventListener('click', linkBtn2);
 
-//
-//
+//FORMULÁRIO
+
+class FormSubmit {
+  constructor(settings) {
+    this.settings = settings;
+    this.form = document.querySelector(settings.form);
+    this.formButton = document.querySelector(settings.button);
+
+    if(this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+  }
+
+  displaySuccess() {
+    this.form.innerHTML = this.settings.success;
+  }
+
+  displayError() {
+    this.form.innerHTML = this.settings.error;
+  }
+
+  init() {
+    if(this.form) this.formButton.addEventListener("click", () => this.displaySuccess());
+    return this;
+  }
+}
+
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  success: "<h1 class='succes'>Mensagem enviada!</h1>",
+  error: "<h1 class='error'>Não foi possivel enviar sua mensagem.</h1>",
+});
+formSubmit.init();
